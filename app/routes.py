4e92e2a92ac2f325f984e8e90a07a3e44271bc73
mwachi21 <mwachidennis567@gmail.com>
2026-02-14 21:16:44 +0300

    Blueprints test

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,21 @@
-from py_compile import main
-from flask import Blueprint, app, send_from_directory
+from flask import Blueprint, send_from_directory
 import random
+import os
 
 # Create a blueprint instance
-api_bp = Blueprint('api', __name__)
+main = Blueprint('main', __name__)
 
-@api_bp.route("/")
+
+@main.route("/")
 def base():
     return send_from_directory('eSoko/public', 'index.html')
+
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @main.route("/<path:path>")
 def home(path):
     return send_from_directory('eSoko/public', path)
+
 
 @main.route("/rand")
 def hello():
